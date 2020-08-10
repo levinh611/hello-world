beeb9363ac383c50067b4707451c21cab2c23054
levinh611 <56534873+levinh611@users.noreply.github.com>
2020-08-11 00:56:47 +0700

    Create ticket.py

---
+++ librarymanage/resources/ticket.py
@@ -0,0 +1,16 @@
+from flask import request, jsonify, abort, make_response
+from flask_restful import Resource, reqparse
+
+from librarymanage.models.borrowbook import Borrowbook
+
+
+class Ticket(Resource):
+    def get(self, id):
+        s = Borrowbook()
+        try:
+            res = s.search(id)
+            if res:
+                return jsonify(res)
+            return make_response(jsonify({"message": "Ticket not found"}), 404)
+        except Exception as e:
+            return make_response(jsonify({"message": str(e)}), 400)
