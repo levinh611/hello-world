5da5e1f840bdeccfa9c39e473688dedf118c8991
levinh611 <56534873+levinh611@users.noreply.github.com>
2020-09-04 11:36:23 +0700

    Create __init__.py

---
+++ LibraryManageFull/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from librarymanage.api import api_v1
+
+
+#from Student.config import *
+
+def create_app() -> Flask:
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = 'secret'
+    return app
+
+app = create_app()
+api_v1.init_app(app)
+
