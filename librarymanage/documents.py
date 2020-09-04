85f0fcecbc8f12b695369a4382c1ede06e68403f
levinh611 <56534873+levinh611@users.noreply.github.com>
2020-09-04 11:44:01 +0700

    Revert "Create documents.py"
    This reverts commit c35a6aaa263c8c89cbf216910bce3f5d17320c1e.

--- librarymanage/documents.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-from flask_swagger_ui import get_swaggerui_blueprint
-SWAGGER_URL ='api/docs'
-API_URL = '/static/documents.yaml'
-
-swaggerui_blueprint = get_swaggerui_blueprint(
-        SWAGGER_URL,
-        API_URL,
-        config={'app_name': "Test App"})
