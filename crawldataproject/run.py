fddf24f6f87b867dceabad50eaa5f8f67fe802fc
levinh611 <56534873+levinh611@users.noreply.github.com>
2020-11-18 02:34:05 +0700

    Create run.py

---
+++ crawldataproject/run.py
@@ -0,0 +1,32 @@
+from crawldataproject import app
+import time
+from flask_apscheduler import APScheduler
+import schedule
+
+if __name__ == '__main__':
+        app.run(debug=True)
+#scheduler = APScheduler()
+
+
+#def run():
+ #       app.run(debug=True)
+
+#def job():
+ #       run()
+
+
+#if __name__ == '__main__':
+ #       schedule.every(5).seconds.do(job)
+#
+  #      while True:
+ #               schedule.run_pending()
+
+
+
+
+
+#scheduler.add_job(id='print_hello', trigger='interval',seconds=10)
+#scheduler.start()
+#while True:
+        #app.run(debug=True)
+        #time.sleep(10)
